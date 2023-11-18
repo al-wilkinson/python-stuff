0f16b2185cd47faf4959415ef28c638838806e9f
al-wilkinson <82517689+al-wilkinson@users.noreply.github.com>
2023-11-19 08:32:27 +1000

    Update ROT-13.py - first pylint pass

--- ROT-13.py
+++ ROT-13.py
@@ -3,20 +3,17 @@ def caesar_shift(text, shift):
     for char in text:
         if char.isalpha():
             # Determine whether the character is upper or lower case
-            is_upper = char.isupper()
-            
+            is_upper = char.isupper()          
             # Shift the character within the range of its case (A-Z or a-z)
-            shifted_char = chr((ord(char) - ord('A' if is_upper else 'a') + shift) % 26 + ord('A' if is_upper else 'a'))
-            
+            shifted_char = chr((ord(char) - ord('A' if is_upper else 'a') + shift) % 26 + ord('A' if is_upper else 'a'))            
             result += shifted_char
         else:
             result += char
-
     return result
 
 # Example usage
-input_text = "WIAOOSFzMjXXBC0KoSKBbJ8puQm5lIEi"
-shift_amount = -13
-encrypted_text = caesar_shift(input_text, shift_amount)
-print("Original text:", input_text)
+INPUT_TEXT = "WIAOOSFzMjXXBC0KoSKBbJ8puQm5lIEi"
+SHIFT_AMOUNT = -13
+encrypted_text = caesar_shift(INPUT_TEXT, SHIFT_AMOUNT)
+print("Original text:", INPUT_TEXT)
 print("Encrypted text:", encrypted_text)
