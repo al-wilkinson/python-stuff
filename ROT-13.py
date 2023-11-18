c33ef216d4f4ed587487ace95ebd6d546baa2939
Al Wilkinson <alastair.wilkinson@wkconsulting.com.au>
2023-11-19 09:05:36 +1000

    More tidying

--- ROT-13.py
+++ ROT-13.py
@@ -1,11 +1,15 @@
+"""Module"""
+#!/usr/bin/env python3
+
 def caesar_shift(text, shift):
+    """Function: perform shift"""
     result = ""
     for char in text:
         if char.isalpha():
             # Determine whether the character is upper or lower case
-            is_upper = char.isupper()          
+            is_upper = char.isupper()
             # Shift the character within the range of its case (A-Z or a-z)
-            shifted_char = chr((ord(char) - ord('A' if is_upper else 'a') + shift) % 26 + ord('A' if is_upper else 'a'))            
+            shifted_char = chr((ord(char) - ord('A' if is_upper else 'a') + shift) % 26 + ord('A' if is_upper else 'a'))
             result += shifted_char
         else:
             result += char
